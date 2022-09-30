6ec618855a5a2693458ad4d9f6146957274137b5
tami <fko2247041@stu.o-hara.ac.jp>
2022-09-30 11:57:07 +0900

    コメントの追加

--- main.cpp
+++ main.cpp
@@ -18,6 +18,8 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 		return -1;			// エラーが起きたら直ちに終了
 	}
 
+	//赤スパありがとうございます。
+
 	// ダブルバッファモード
 	SetDrawScreen(DX_SCREEN_BACK);
 
